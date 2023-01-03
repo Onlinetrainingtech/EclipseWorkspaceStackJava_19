f29ec4e14b3027fdb7ac9ae5fb377694a26dce61
OnlineTrainer <mohamedtechdata@gmail.com>
2023-01-03 07:51:05 +0530

    Initial commit

---
+++ ProjectP2/src/Demo.java
@@ -0,0 +1,21 @@
+
+public class Demo 
+{
+    void get1()
+    {
+    	System.out.println("Get1 function..");
+    	System.out.println("*****************");
+    }
+    void get2()
+    {
+    	System.out.println("Get2 function..");
+    }
+	public static void main(String[] args)
+	{
+		
+          Demo f1=new Demo();//Object creation
+          f1.get1();
+          f1.get2();
+	}
+
+}
